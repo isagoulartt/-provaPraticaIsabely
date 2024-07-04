379ae8d9c9993c87dc42e6b364bc94a5cba5309f
ISABELY AMABILE GOULART <isabely_goulart@estudante.sesisenai.org.br>
2024-07-04 11:49:10 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header>
+        <img className= "logo" src="senai-logo-1.png"></img>  
+        <h1> Isabely Amábile Goulart 3°c</h1>
+        </header>
+    )
+}
